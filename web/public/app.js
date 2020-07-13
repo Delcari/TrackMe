0fc3cbbe2528ea965fb31e9350c8facc93e4c05e
Delcari <delcari@protonmail.com>
2020-07-13 16:46:57 +1000

    NEW: Add server files

---
+++ web/public/app.js
@@ -0,0 +1,29 @@
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+//Adds all the devices from the devices array to the table
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+    </tr>`
+    );
+});
+
+//Adds the device to the devices array when the save button is clicked
+$("#add-device").on("click", function () {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    
+    devices.push({user, name});
+    localStorage.setItem('devices', JSON.stringify(devices))
+
+    location.href = 'device-list.html';
+});
+
+//Send command, prints the user input to console when the send button in pressed
+$('#send-command').on("click", function () {
+    const command = $('#command').val();
+
+    console.log(`command is ${command}`);
+}); 
