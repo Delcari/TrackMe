0adadc26b0cbb99a3710cc1c1d08f27ecd02c61c
Delcari <delcari@protonmail.com>
2020-07-13 15:57:43 +1000

    NEW: Add form functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ devices.push({ user: "Mary", name: "Mary's Iphone"});
 devices.push({ user: "Alex", name: "Alex's Surface Pro"});
 devices.push({ user: "Mary", name: "Mary's Macbook" });
 
+//Adds all the devices from the devices array to the table
 devices.forEach(function(device) {
     $('#devices tbody').append(`
     <tr>
@@ -11,4 +12,13 @@ devices.forEach(function(device) {
         <td>${device.name}</td>
     </tr>`
     );
+});
+
+//Adds the device to the devices array when the save button is clicked
+document.querySelector("#add-device").addEventListener('click', function() {
+    const user = document.querySelector('#user').value;
+    const name = document.querySelector('#name').value;
+
+    devices.push({user:user, name:name});
+    console.log(devices);
 });
