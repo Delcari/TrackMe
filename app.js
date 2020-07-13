bbcb4b8a9cf5399e00b38cd1b877722d19edc211
Delcari <delcari@protonmail.com>
2020-07-13 15:40:33 +1000

    ENHANCE: Simplify filling table

--- app.js
+++ app.js
@@ -5,19 +5,10 @@ devices.push({ user: "Alex", name: "Alex's Surface Pro"});
 devices.push({ user: "Mary", name: "Mary's Macbook" });
 
 devices.forEach(function(device) {
-    const table = document.querySelector('#devices');
-    const row = document.createElement('tr');
-
-    const user = document.createElement('td');
-    const userText = document.createTextNode(device.user);
-    user.appendChild(userText);
-
-    const name = document.createElement('td');
-    const nameText = document.createTextNode(device.name);
-    name.appendChild(nameText);
-
-    row.appendChild(user);
-    row.appendChild(name);
-
-    table.appendChild(row);
+    $('#devices tbody').append(`
+    <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+    </tr>`
+    );
 });
