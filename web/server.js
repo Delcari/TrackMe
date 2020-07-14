3ccb9254b9a81be73b3365d31a9f3253ef867cb0
Delcari <delcari@protonmail.com>
2020-07-14 20:06:19 +1000

    NEW: Add routes

--- web/server.js
+++ web/server.js
@@ -16,6 +16,14 @@ app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
 
+app.get('/registration', (req, res) => {
+	res.sendFile(`${base}/registration.html`)
+});
+
+app.get('/login', (req, res) => {
+	res.sendFile(`${base}/login.html`);
+});
+
 app.get('/register-device', (req, res) => {
 	res.sendFile(`${base}/register-device.html`);
 });
