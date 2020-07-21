bf612c49ffb77fc80c61140b896e5cbb67afd3ec
Delcari <delcari@protonmail.com>
2020-07-21 13:06:16 +1000

    NEW: APi Requests

--- api/api.js
+++ api/api.js
@@ -7,6 +7,34 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser:true, useUnifiedTopolog
 
 
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.post('/api/devices', (req, res) => {
+    const { name, user, sensorData } = req.body;
+    const newDevice = new Device({
+        name,
+        user,
+        sensorData
+    });
+    newDevice.save(err => {
+        return err 
+        ? res.send(err)
+        : res.send("Successfully added device and data");
+    })
+});
+
+app.post('/api/send-command', (req, res) => {
+    console.log(req.body);
+})
+
 const port = process.env.PORT || 5000;
 
 app.get('/api/test', (req, res) => {
@@ -21,6 +49,7 @@ app.get('/api/devices', (req, res) => {
             : res.send(devices);
     });
 });
+
 
 app.listen(port, () =>
 {
